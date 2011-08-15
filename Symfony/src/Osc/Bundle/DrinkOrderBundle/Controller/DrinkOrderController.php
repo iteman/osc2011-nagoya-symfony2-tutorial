3c4a55799a02b21ecec057bb40525fd9483290fa
KUBO Atsuhiro <kubo@iteman.jp>
2011-08-15 21:35:19 +0900

    商品選択フォームの表示を実装した。

--- Symfony/src/Osc/Bundle/DrinkOrderBundle/Controller/DrinkOrderController.php
+++ Symfony/src/Osc/Bundle/DrinkOrderBundle/Controller/DrinkOrderController.php
@@ -4,11 +4,16 @@ namespace Osc\Bundle\DrinkOrderBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Osc\Bundle\DrinkOrderBundle\Entity\DrinkOrder;
 
 class DrinkOrderController extends Controller
 {
     public function productAction()
     {
-        return $this->render('OscDrinkOrderBundle:DrinkOrder:product.html.twig');
+        $form = $this->createFormBuilder(new DrinkOrder())
+            ->add('product_id', 'choice', array( 'choices' => array('1' => 'BlueBull 128個入ケース', '2' => 'GreenBull 128個入ケース')))
+            ->add('quantity', 'text')
+            ->getForm();
+        return $this->render('OscDrinkOrderBundle:DrinkOrder:product.html.twig', array('form' => $form->createView()));
     }
 }
