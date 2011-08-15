ad15c59f56dd3ee99429079d71f88c3b78a9ce8c
KUBO Atsuhiro <kubo@iteman.jp>
2011-08-15 21:08:21 +0900

    app/console doctrine:generate:entity コマンドによる DrinkOrder エンティティとマッピングファイルを追加した。

---
+++ Symfony/src/Osc/Bundle/DrinkOrderBundle/Entity/DrinkOrder.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Osc\Bundle\DrinkOrderBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Osc\Bundle\DrinkOrderBundle\Entity\DrinkOrder
+ */
+class DrinkOrder
+{
+    /**
+     * @var integer $id
+     */
+    private $id;
+
+    /**
+     * @var integer $product_id
+     */
+    private $product_id;
+
+    /**
+     * @var integer $quantity
+     */
+    private $quantity;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set product_id
+     *
+     * @param integer $productId
+     */
+    public function setProductId($productId)
+    {
+        $this->product_id = $productId;
+    }
+
+    /**
+     * Get product_id
+     *
+     * @return integer
+     */
+    public function getProductId()
+    {
+        return $this->product_id;
+    }
+
+    /**
+     * Set quantity
+     *
+     * @param integer $quantity
+     */
+    public function setQuantity($quantity)
+    {
+        $this->quantity = $quantity;
+    }
+
+    /**
+     * Get quantity
+     *
+     * @return integer
+     */
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+}
